3eb2149a05c5c43a1af3d1f1e99d3270dae4edf6
sch <767718235@qq.com>
2019-11-17 11:10:11 +0800

    抓取歌手数据+数据处理+封装singer

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,11 +1,84 @@
 <template>
-    <div>
+    <div class="singer">
         歌手页
     </div>
 </template>
 <script>
-export default {};
+import {getSingerList} from 'api/singers';
+import {ERR_OK} from 'api/config';
+import Singer from 'common/js/singer';
+
+const HOT_NAME = '热门';
+const HOT_SINGER_LEN = 10;
+
+export default {
+  data() {
+    return {
+      singerList: []
+    };
+  },
+  created() {
+    this._getSingerList();
+  },
+  methods: {
+    _getSingerList() {
+      getSingerList().then((res) => {
+        if (res.code === ERR_OK) {
+          this.singerList = res.data.list;
+          this._normalizeSinger(this.singerList);
+        }
+      });
+    },
+    _normalizeSinger(list) {
+      let map = {
+        hot: {
+          title: HOT_NAME,
+          items: []
+        }
+      };
+      list.forEach((item, index) => {
+        if (index < HOT_SINGER_LEN) {
+          map.hot.items.push(new Singer({
+            id: item.Fsinger_mid,
+            name: item.Fsinger_name
+          }));
+        }
+
+        const key = item.Findex;
+        if (!map[key]) {
+          map[key] = {
+            title: key,
+            items: []
+          };
+        }
+        map[key].items.push(new Singer({
+          id: item.Fsinger_mid,
+          name: item.Fsinger_name
+        }));
+      });
+
+      let hot = [];
+      let ret = [];
+      for (let key in map) {
+        let val = map[key];
+        if (val.title.match(/[a-zA-Z]/)) {
+          ret.push(val);
+        } else if (val.title === HOT_NAME) {
+          hot.push(val);
+        }
+      }
+      ret.sort((a, b) => {
+        return a.title.charCodeAt(0) - b.title.charCodeAt(0);
+      });
+      console.log(hot.concat(ret));
+    }
+  }
+};
 </script>
 <style lang="stylus" scoped>
-
+  .singer
+    position: fixed;
+    top: 88px;
+    bottom: 0;
+    widthL 100%
 </style>
